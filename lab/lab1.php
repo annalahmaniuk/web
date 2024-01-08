2d5efa8ba6f8ec4f8bcdf23aca5b1f9240c8e019
Hanna Lakhmaniuk <91181821+annalahmaniuk@users.noreply.github.com>
2024-01-09 00:02:44 +0200

    Update lab1.php

--- lab/lab1.php
+++ lab/lab1.php
@@ -5,17 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>lab1</title>
-        </head>
     <style>
         form {
             text-align: center;
         }
     </style>
+</head>
 <body>
-<form action="lab.php" method="post">
-    <input type="text" id ="note" name="note"><br>
-    <p>Виберіть дату: <br> <input type="date" name="calendar"><br>
-    <input type="submit">
-</form>
+    <form action="lab.php" method="post">
+        <label for="note">Введіть нотатку:</label><br>
+        <input type="text" id="note" name="note"><br>
+        <label for="calendar">Виберіть дату:</label><br>
+        <input type="date" id="calendar" name="calendar"><br>
+        <input type="submit" value="Відправити">
+    </form>
 </body>
 </html>
+
